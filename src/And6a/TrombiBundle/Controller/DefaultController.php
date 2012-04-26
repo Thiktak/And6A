ebb3f44b5a5e0b812edece3f53f9fba3fa314736
Olivars Georges <olivares.georges@gmail.com>
2012-04-26 15:33:19 +0200

    TPL - First try

--- src/And6a/TrombiBundle/Controller/DefaultController.php
+++ src/And6a/TrombiBundle/Controller/DefaultController.php
@@ -12,4 +12,27 @@ class DefaultController extends Controller
     {
         return $this->render('And6aTrombiBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function userAction($user, $style = 'badge', $withLink = true)
+    {
+    	if( !is_object($user) )
+    		$user = array(
+    			'id'     => $user,
+    			'name'   => 'Olivarès',
+    			'fname'  => 'Georges',
+    			'class'  => array(
+	    			'name' => 'Informatique & Réseaux',
+	    			'salt' => 'IR',
+	    		),
+	    		'year'   => 1,
+	    		'promo'  => date('Y'),
+    			'avatar' => 'https://secure.gravatar.com/avatar/4d880c313c08c2a03ff877647e9ad770?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png',
+	    	);
+    	
+    	return $this->render('And6aTrombiBundle:Default:user.html.twig', array(
+    		'user'  => $user,
+    		'style' => $style,
+    		'withLink' => $withLink,
+	    ));
+    }
 }
